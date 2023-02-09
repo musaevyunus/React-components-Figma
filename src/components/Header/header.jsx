77fb0bbde1e7e7eab8db23d2a1ac3f6a9c58c264
musaevyunus <yunus.musaev095@gmail.com>
2023-02-09 11:34:36 +0300

    Обновил

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 
-export default function Header(){
-    return (
-        <>
-        <div className="header">
-            <div className="c"><img src="Group 1.png" alt="gr"/></div>
-            <div className="hrefs"> <a href="" className="hrefshead">
-            <div className="glav"><a href="" className="glavhref">Главная</a></div>
-            <div className="onas"><a href="" className="onashref">О нас</a></div>
-            <div className="kontakty"><a href="" className="konthref">Контакты</a></div></a>
-            </div>
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="c">
+          <img src="Group 1.png" alt="gr" />
         </div>
-            <hr className="hr" />
-            </>
-    )
+        <div className="hrefs">
+          {" "}
+          <a href="" className="glavhref">
+            Главная
+          </a>
+          <a href="" className="glavhref">
+            О нас
+          </a>
+          <a href="" className="glavhref">
+            Контакты
+          </a>
+        </div>
+      </div>
+      <hr className="hr" />
+    </>
+  );
 }
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import './main.css';
-
+import React from "react";
+import "./main.css";
 
 export default function Main() {
-    return (
-        <div className='main'>
-        <div className='zero'>0</div>
-        <div className='but'>
-        <div className='buttons'>
-            <button className='uvel'>Увеличить</button>
-            <button className='umen'>Уменьшить</button>
-            <button className='sbros'>Сброс</button>
+  return (
+    <div className="main">
+      <div className="zero">0</div>
+      
+        <div className="buttons">
+          <button className="uvel">Увеличить</button>
+          <button className="umen">Уменьшить</button>
+          <button className="sbros">Сброс</button>
         </div>
-        </div>
-        
-        </div>
-    )
+      </div>
+   
+  );
 }
